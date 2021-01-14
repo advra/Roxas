c5b573ca5196e5b00e3a417d7b6c366573e0c4f3
Adrian D. Alonzo <aalonzo3@gatech.edu>
2021-01-14 11:20:20 -0800

    Updated gitignore

--- src/main/java/com/github/advra/roxas/ClientInstance.java
+++ src/main/java/com/github/advra/roxas/ClientInstance.java
@@ -6,8 +6,13 @@ import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.User;
+import discord4j.core.object.reaction.ReactionEmoji;
+
+import java.io.File;
+import java.util.logging.Logger;
 
 public class Bot {
+
 
     public static void main(String[] args) {
         GatewayDiscordClient client = DiscordClientBuilder.create("Nzk5MDMxOTIyMTAwNTM1MzQ2.X_9qdw.KLrWnQtILuKVGG8cWvKY3YBx4JI").build().login().block();
@@ -17,14 +22,6 @@ public class Bot {
                     User self = event.getSelf();
                     System.out.println(String.format("Logged in as %s#%s", self.getUsername(), self.getDiscriminator()));
                 });
-
-        client.getEventDispatcher().on(MessageCreateEvent.class)
-                .map(MessageCreateEvent::getMessage)
-                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
-                .filter(message -> message.getContent().equalsIgnoreCase("!ping"))
-                .flatMap(Message::getChannel)
-                .flatMap(channel -> channel.createMessage("Pong!"))
-                .subscribe();
 
         client.onDisconnect().block();
     }
