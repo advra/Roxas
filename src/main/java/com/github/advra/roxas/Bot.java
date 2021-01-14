e2d45ad7a694906e36047766b2417ea643bcbf83
Adrian D. Alonzo <aalonzo3@gatech.edu>
2021-01-13 16:54:54 -0800

    Initial ping pong example

---
+++ src/main/java/com/github/advra/roxas/Bot.java
@@ -0,0 +1,32 @@
+package com.github.advra.roxas;
+
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+
+public class Bot {
+
+    public static void main(String[] args) {
+        GatewayDiscordClient client = DiscordClientBuilder.create("Nzk5MDMxOTIyMTAwNTM1MzQ2.X_9qdw.KLrWnQtILuKVGG8cWvKY3YBx4JI").build().login().block();
+
+        client.getEventDispatcher().on(ReadyEvent.class)
+                .subscribe(event -> {
+                    User self = event.getSelf();
+                    System.out.println(String.format("Logged in as %s#%s", self.getUsername(), self.getDiscriminator()));
+                });
+
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+                .filter(message -> message.getContent().equalsIgnoreCase("!ping"))
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("Pong!"))
+                .subscribe();
+
+        client.onDisconnect().block();
+    }
+
+}
