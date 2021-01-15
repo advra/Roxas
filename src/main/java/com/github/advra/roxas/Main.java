012118aae4ad349703a8151169ba4fb76927b0bd
Adrian D. Alonzo <aalonzo3@gatech.edu>
2021-01-14 23:10:19 -0800

    Added ping pong example

--- src/main/java/com/github/advra/roxas/Main.java
+++ src/main/java/com/github/advra/roxas/Main.java
@@ -6,25 +6,32 @@ import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.User;
+import discord4j.core.DiscordClient;
 
 import org.aeonbits.owner.Config;
 import org.aeonbits.owner.ConfigFactory;
 
-public class ClientInstance {
-    //Nzk5MDMxOTIyMTAwNTM1MzQ2.X_9qdw.KLrWnQtILuKVGG8cWvKY3YBx4JI
-    public static void main(String[] args) {
-        final String token = "Nzk5MDMxOTIyMTAwNTM1MzQ2.X_9qdw.KLrWnQtILuKVGG8cWvKY3YBx4JI";
+import java.util.HashMap;
+import java.util.Map;
 
+public class Main {
+
+    private static final Map<String, Command> commands = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        // load token from properties
         ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
         try{
             System.out.println("Roxas Server booted " + cfg.hostname() + ":" + cfg.port() +
-                    " will run " + cfg.maxThreads() + " with key: " + cfg.token());
+                    " will run " + cfg.maxThreads() + " threads with key: " + cfg.token());
         }catch(Exception e){
             e.printStackTrace();
         }
 
+        // Initiate main client
         final GatewayDiscordClient client = DiscordClientBuilder
-                .create(token)
+                .create(cfg.token())
                 .build()
                 .login()
                 .block();
@@ -34,6 +41,14 @@ public class ClientInstance {
                     User self = event.getSelf();
                     System.out.println(String.format("Logged in as %s#%s", self.getUsername(), self.getDiscriminator()));
                 });
+
+//        client.getEventDispatcher().on(MessageCreateEvent.class)
+//                .map(MessageCreateEvent::getMessage)
+//                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+//                .filter(message -> message.getContent().equalsIgnoreCase("!ping"))
+//                .flatMap(Message::getChannel)
+//                .flatMap(channel -> channel.createMessage("Pong!"))
+//                .subscribe();
 
         client.onDisconnect().block();
     }
