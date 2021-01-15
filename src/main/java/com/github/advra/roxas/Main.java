2a888b95f286b84a204e7d2fdf9394cddbf5374e
Adrian D. Alonzo <aalonzo3@gatech.edu>
2021-01-15 00:12:36 -0800

    Fixed pingpong using imperative approach

--- src/main/java/com/github/advra/roxas/Main.java
+++ src/main/java/com/github/advra/roxas/Main.java
@@ -1,5 +1,8 @@
 package com.github.advra.roxas;
 
+import com.github.advra.roxas.CommandHandler;
+import com.github.advra.roxas.Command;
+
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
@@ -10,6 +13,8 @@ import discord4j.core.DiscordClient;
 
 import org.aeonbits.owner.Config;
 import org.aeonbits.owner.ConfigFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +22,12 @@ import java.util.Map;
 public class Main {
 
     private static final Map<String, Command> commands = new HashMap<>();
+
+    static {
+        commands.put("ping", event -> event.getMessage()
+                .getChannel().block()
+                .createMessage("Pong!").block());
+    }
 
     public static void main(String[] args) {
 
@@ -36,6 +47,9 @@ public class Main {
                 .login()
                 .block();
 
+        // register events
+
+
         client.getEventDispatcher().on(ReadyEvent.class)
                 .subscribe(event -> {
                     User self = event.getSelf();
@@ -49,6 +63,21 @@ public class Main {
 //                .flatMap(Message::getChannel)
 //                .flatMap(channel -> channel.createMessage("Pong!"))
 //                .subscribe();
+
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                // subscribe is like block, in that it will *request* for action
+                // to be done, but instead of blocking the thread, waiting for it
+                // to finish, it will just execute the results asynchronously.
+                .subscribe(event -> {
+                    final String content = event.getMessage().getContent(); // 3.1 Message.getContent() is a String
+                    for (final Map.Entry<String, Command> entry : commands.entrySet()) {
+                        // We will be using ! as our "prefix" to any command in the system.
+                        if (content.startsWith('!' + entry.getKey())) {
+                            entry.getValue().execute(event);
+                            break;
+                        }
+                    }
+                });
 
         client.onDisconnect().block();
     }
